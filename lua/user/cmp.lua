ecbf4d53837776cd0ad9368978ce4b605054ff84
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2022-06-09 18:30:09 +0300

    Disable cmp in comments

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -46,6 +46,15 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
+  enabled = function()
+    local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+    if in_prompt then -- this will disable cmp in the Telescope window (taken from the default config)
+      return false
+    end
+    local context = require("cmp.config.context")
+    return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+  end,
+
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
