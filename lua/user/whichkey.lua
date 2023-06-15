7607b894a5bbb1df13da21039ae062c1be0796d1
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2023-06-15 06:04:39 +0300

    Disable packer whickey shortcuts

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -142,14 +142,14 @@ local mappings = {
 	["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
 	["P"] = { "<cmd>Telescope projects<cr>", "Projects" },
 
-	p = {
-		name = "Packer",
-		c = { "<cmd>PackerCompile<cr>", "Compile" },
-		i = { "<cmd>PackerInstall<cr>", "Install" },
-		s = { "<cmd>PackerSync<cr>", "Sync" },
-		S = { "<cmd>PackerStatus<cr>", "Status" },
-		u = { "<cmd>PackerUpdate<cr>", "Update" },
-	},
+	-- p = {
+	-- 	name = "Packer",
+	-- 	c = { "<cmd>PackerCompile<cr>", "Compile" },
+	-- 	i = { "<cmd>PackerInstall<cr>", "Install" },
+	-- 	s = { "<cmd>PackerSync<cr>", "Sync" },
+	-- 	S = { "<cmd>PackerStatus<cr>", "Status" },
+	-- 	u = { "<cmd>PackerUpdate<cr>", "Update" },
+	-- },
 
 	g = {
 		name = "Git",
