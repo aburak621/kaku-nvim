fc7096cc04fe161625f76604c957e6641406325d
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-08-11 15:34:59 +0300

    Formatting

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,40 +9,39 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- Web
-		formatting.prettier.with({
-			filetypes = {
-				"html",
-				"css",
-				"json",
-				"markdown",
-				-- "javascript",
-				-- "typescript",
-				-- "typescriptreact",
-				-- "yaml",
-				-- "vue",
-				-- "scss",
-				-- "less",
-				-- "jsonc",
-				-- "graphql",
-				-- "handlebars",
-			},
-			extra_args = { "--semi", "--single-quote", "--jsx-single-quote" },
-		}),
-        require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
-		-- formatting.rustfmt,
-		-- Python
-		-- formatting.black,
-		-- diagnostics.flake8,
-		-- diagnostics.pylint,
-		-- Lua
-		formatting.stylua.with({
-			args = { "--indent-type", "Spaces" },
-		}),
-		formatting.clang_format.with({
-			extra_args = { "--style=Microsoft" },
-		}),
-	},
+    debug = false,
+    sources = {
+        -- Web
+        formatting.prettier.with({
+            filetypes = {
+                "html",
+                "css",
+                "json",
+                "markdown",
+                "javascript",
+                -- "typescript",
+                -- "typescriptreact",
+                -- "yaml",
+                -- "vue",
+                -- "scss",
+                -- "less",
+                -- "jsonc",
+                -- "graphql",
+                -- "handlebars",
+            },
+            extra_args = { "--semi", "--single-quote", "--jsx-single-quote" },
+        }),
+        -- formatting.rustfmt,
+        -- Python
+        -- formatting.black,
+        -- diagnostics.flake8,
+        -- diagnostics.pylint,
+        -- Lua
+        formatting.stylua.with({
+            args = { "--indent-type", "Spaces" },
+        }),
+        formatting.clang_format.with({
+            extra_args = { "--style=Microsoft" },
+        }),
+    },
 })
