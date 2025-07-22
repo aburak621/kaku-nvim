30e58ac6cfe0c9429bf3f74b034042ff9c123da3
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2025-07-22 23:10:29 +0300

    Modify capabilities from kickstart

--- lua/user/mason.lua
+++ lua/user/mason.lua
@@ -66,7 +66,8 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 capabilities["workspace"] = { didChangeWatchedFiles = { dynamicRegistration = true }}
 local lspconfig = require("lspconfig")
 
