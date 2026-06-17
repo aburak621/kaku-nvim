80b09552a8dc83af06c97e9319002b2ef705609f
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2026-06-17 14:00:07 +0300

    Use x mode for _dP instead of v mode to prevent pasting when typing p on snippets

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -79,7 +79,7 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+keymap("x", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
